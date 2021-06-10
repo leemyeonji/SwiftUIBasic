5a5a832b8285027dab91a603c4254a3aa5860dcd
leemyeonji <61834038+leemyeonji@users.noreply.github.com>
2021-06-10 17:45:29 +0900

    First setting

---
+++ SwiftUIBasic/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 임현지 on 2021/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Leemyeonji")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBasic/Shared/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  Shared
+//
+//  Created by 임현지 on 2021/06/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
